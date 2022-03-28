900df4d74736ef3dc34df647e8bad8316acf52d4
Askar312 <askar.kg.com@mail.ru>
2022-03-28 13:47:58 +0600

    express

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import mongoose from "mongoose";
+import router from "./routes.js";
+import fileUpload from "express-fileupload";
+import cors from "cors";
+
+const PORT = 8000;
+const DB_URL = `mongodb+srv://user:user@cluster0.ybahg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(express.static("image"));
+app.use(fileUpload({}));
+app.use("/api", router);
+
+async function startApp() {
+  try {
+    await mongoose.connect(DB_URL);
+    app.listen(PORT, () => console.log("Server " + PORT));
+  } catch (e) {
+    console.log(e);
+  }
+}
+startApp();
